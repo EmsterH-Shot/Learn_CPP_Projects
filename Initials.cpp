35911b3b3c0d7638eb6dee5eecfda5f181444794
em_henders <100213017+EmsterH-Shot@users.noreply.github.com>
2025-09-01 15:27:20 -0400

    Create Initials.cpp

---
+++ Initials.cpp
@@ -0,0 +1,16 @@
+// I like owls! (practicing comments)
+
+#include <iostream>
+
+// Prints my first and last name initials.
+int main() {
+
+ std::cout << "EEEEE   H   H\n";
+ std::cout << "E       H   H\n";
+ std::cout << "E.      H   H\n";
+ std::cout << "EEE     HHHHH\n";
+ std::cout << "E       H   H\n";
+ std::cout << "E       H   H\n";
+ std::cout << "EEEEE   H   H\n";
+
+}
