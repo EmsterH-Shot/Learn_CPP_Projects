7b509ca950b6a8a3e42deec15d23302337ca5cad
EmsterH-Shot <emhenders17@gmail.com>
2025-09-03 13:40:17 -0400

    create fizzbuzz.cpp and text_adventure.cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// Solves the classic technical interview problem (according to Codecademy at the time), based on the Fizz Buzz children's game.
+int main() {
+
+    for (int i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 != 0) {
+
+        std::cout << "Fizz\n";
+
+    } else if (i % 5 == 0 && i % 3 != 0) {
+
+        std::cout << "Buzz\n";
+
+    } else if (i % 3 == 0 && i % 5 == 0) {
+
+        std::cout << "FizzBuzz\n";
+    } else
+
+        std::cout << i << "\n";
+
+
+    }
+
+}
