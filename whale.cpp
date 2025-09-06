ee93ff1a8bcfbc2176dc612c23d583e9db5b4c4d
EmsterH-Shot <emhenders17@gmail.com>
2025-09-06 10:29:00 -0400

    Fix formatting issues in whale.cpp

--- whale.cpp
+++ whale.cpp
@@ -7,36 +7,36 @@ int main(){
 
     std::string input = "turpentine and turtles";
 
+    std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
 
- std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+    std::vector<char> result;
 
+    for (int i = 0; i < input.size(); i++) {
+        
+        for (int j = 0; j < vowels.size(); j++) {
+            
+            if (input[i] == vowels[j]) {
+            
+                result.push_back(input[i]);
+            
+            } 
+            
+            if (input[i] == 'u' || input[i] == 'e') {
+            
+                result.push_back(input[i]);
+            
+            }
 
- std::vector<char> result;
+        }
 
+    }
 
- for (int i = 0; i < input.size(); i++) {
-   for (int j = 0; j < vowels.size(); j++) {
-     if (input[i] == vowels[j]) {
-       result.push_back(input[i]);
-   }   if (input[i] == 'u' || input[i] == 'e') {
+    for (int k = 0; k < result.size(); k++) {
+        
+        std::cout << result[k];
+    
+    }
 
+    std::cout << "\n";
 
-       result.push_back(input[i]);
-
-
-}
-   }
- }
-
-
- for (int k = 0; k < result.size(); k++) {
-
-
-   std::cout << result[k];
-
-
- }
-
-
- std::cout << "\n";
 }
